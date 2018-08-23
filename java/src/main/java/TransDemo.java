43cc67890ba61bb739bc69b7fdf1bee8f13a2585
ljcyu <ljcyu@163.com>
2018-08-23 19:03:44 +0800

    其他几个使用事务的方法

--- java/src/main/java/TransDemo.java
+++ java/src/main/java/TransDemo.java
@@ -1,8 +1,6 @@
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Pipeline;
-import redis.clients.jedis.Response;
-import redis.clients.jedis.Transaction;
+import redis.clients.jedis.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -13,7 +11,9 @@ import java.util.concurrent.TimeUnit;
 public class TransDemo {
     public static void main(String[] args) throws Exception{
         //noTrans();
-        trans();
+        //trans();
+        //pipelineTest();
+        pipelineTransTest();
         /*Jedis redis=new Jedis("localhost");
         redis.incr("notrans:");
         System.out.println(redis.decr("notrans:"));*/
@@ -62,5 +62,79 @@ public class TransDemo {
             new Thread(r).start();
         }
         TimeUnit.MILLISECONDS.sleep(500);
+    }
+    public static void pipelineTest(){
+        Jedis redis=new Jedis("localhost");
+        Pipeline pipeline=redis.pipelined();
+        for(int i=0;i<10;i++){
+            String key=String.format("test-%d:",i);
+            pipeline.set(key,""+i);
+            pipeline.expire(key,5);
+        }
+        List<Object> resList=pipeline.syncAndReturnAll();
+        resList.forEach(System.out::println);
+        redis.disconnect();
+    }
+    public static void pipelineTransTest(){
+        Jedis redis=new Jedis("localhost");
+        Pipeline pipeline=redis.pipelined();
+        pipeline.multi();
+        for(int i=0;i<10;i++){
+            String key=String.format("test-%d:",i);
+            pipeline.set(key,""+i);
+            pipeline.expire(key,5);
+        }
+        pipeline.exec();
+        List<Object> resList=pipeline.syncAndReturnAll();
+        resList.forEach(System.out::println);
+        redis.disconnect();
+    }
+    //同时连接多个主机
+    public void multiJedisShardTest() {
+        List<JedisShardInfo> shards = Arrays.asList(new JedisShardInfo("localhost", 6379),
+                new JedisShardInfo("localhost", 6380));
+        ShardedJedis sharding = new ShardedJedis(shards);
+        for (int i = 0; i < 1000; i++) {
+            sharding.set("multiJedis-key-" + i, "value-" + i);
+        }
+        sharding.disconnect();
+    }
+    //同时连接多个主机,pipeline方式执行
+    public void multiJedisShardPipeLineTest() {
+        List<JedisShardInfo> shards = Arrays.asList(new JedisShardInfo("localhost", 6379),
+                new JedisShardInfo("localhost", 6380));
+        ShardedJedis sharding = new ShardedJedis(shards);
+        ShardedJedisPipeline pipelined = sharding.pipelined();
+        for (int i = 0; i < 1000; i++) {
+            pipelined.set("multiJedis-key-" + i, "value-" + i);
+        }
+        List<Object> resList=pipelined.syncAndReturnAll();
+        resList.forEach(System.out::println);
+        sharding.disconnect();
+    }
+    //线程池方式同时连接多个主机
+    public void multiJedisShardThreadTest() {
+        List<JedisShardInfo> shards = Arrays.asList(new JedisShardInfo("localhost", 6379),
+                new JedisShardInfo("localhost", 6380));
+        ShardedJedisPool pool = new ShardedJedisPool(new JedisPoolConfig(), shards);
+        ShardedJedis sharding = pool.getResource();
+        for (int i = 0; i < 1000; i++) {
+            sharding.set("multiJedis-key-" + i, "value-" + i);
+        }
+        pool.destroy();
+    }
+    //同时连接多个主机,pipeline方式执行
+    public void multiJedisShardPipeLineThreadTest() {
+        List<JedisShardInfo> shards = Arrays.asList(new JedisShardInfo("localhost", 6379),
+                new JedisShardInfo("localhost", 6380));
+        ShardedJedisPool pool = new ShardedJedisPool(new JedisPoolConfig(), shards);
+        ShardedJedis sharding = pool.getResource();
+        ShardedJedisPipeline pipelined = sharding.pipelined();
+        for (int i = 0; i < 1000; i++) {
+            pipelined.set("multiJedis-key-" + i, "value-" + i);
+        }
+        List<Object> resList=pipelined.syncAndReturnAll();
+        resList.forEach(System.out::println);
+        pool.destroy();
     }
 }
